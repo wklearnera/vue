69863a05c1fb3defb8683c1eec45307c2caf7a7f
wklearnera <33716502@qq.com>
2021-02-27 10:17:13 +0800

    setting

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 产品构建信息是否提示
 Vue.config.productionTip = false
 
+// new Vue({
+//     el = '#app',
+//     render: function(h) {
+//         return h(App)
+//     }
+// })
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
+
 }).$mount('#app')
